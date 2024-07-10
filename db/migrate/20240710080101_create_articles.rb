d6885ced3496bda92a5ab08e3e061c37f9ba03aa
Aniruddha <verticalfox@Verticalfoxs-MacBook-Pro-2.local>
2024-07-10 19:07:08 +0530

    adding stundent table

--- db/migrate/20240710080101_create_articles.rb
+++ db/migrate/20240710080101_create_articles.rb
@@ -2,9 +2,7 @@ class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
       t.string :title
-      
       t.text :specs
-
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_10_080101) do
+ActiveRecord::Schema.define(version: 2024_07_10_132045) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "specs"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "student_tables", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.text "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
